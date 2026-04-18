6b6cf372e6ae5c314e3748079eff55da4c0e7a94
Jonathan Alcineus <jha21a@fsu.edu>
2026-04-18 14:00:11 -0400

    Starting to build the neurons based on the original paper

--- gfs.py
+++ gfs.py
@@ -10,26 +10,43 @@ from brian2 import *
 import matplotlib
 import numpy
 
-class gfs:
+class gfs_object:
     # Paper decided to experiment with using 4 neurons
     # I wanted there to be an option to change the number of neurons within
     # the GFS, so I decided to have the default number of neurons match the paper
     # but I could experiment with more neurons if I wanted to
 
-    def __init__(self, num_gf = 1, num_ttm = 1, num_psi = 1, num_dlmn = 1):
-        self.num_gf = num_gf # Number of GF neurons in this GFS, default is 1
-        self.num_ttm = num_ttm # Number of TTMn neurons in this GFS, default is 1
-        self.num_psi = num_psi # Number of PSI neurons in this GFS, default is 1
-        self.num_dlmn = num_dlmn # Number of DLMn neurons in this GFS, default is 1
+    # num_gf = 1, num_ttm = 1, num_psi = 1, num_dlmn = 1, Save these just in case for the number of neurons if want to change
+    #         self.num_gf = num_gf # Number of GF neurons in this GFS, default is 1
+    #   self.num_ttm = num_ttm # Number of TTMn neurons in this GFS, default is 1
+    #   self.num_psi = num_psi # Number of PSI neurons in this GFS, default is 1
+    #   self.num_dlmn = num_dlmn # Number of DLMn neurons in this GFS, default is 1
+    # We'll have to use the Spatial Neuron class that only covers one Neurons
 
-    # Sample equation for the neuron's groups, will change later
+    def __init__(self):
+        # Now we are getting to describe the shapes for each of th eneurons, this is according to page 3 of the ENEURO Paper
+
+        # First we are putting the default morphology (or shape) for the gf neuron
+        # The GF neuron does not contains any axons or dendrites, so nothing else needed other than using Soma class
+        # But it does have electrical synpases between the axon of the PSI and the dendrite of the TTmn
+        self.gf_neuron_morph = Soma(diameter = 8*um, length=400*um)
+
+        # Here is the morphology for the TTMn neuron, it contains two dendrites and one active axon
+        self.ttmn_neuron_morph = Soma(diameter=6*um)
+        self.ttmn_neuron_morph.medial_dendrite = Cylinder(length=60*um)
+        self.ttmn_neuron_morph.lateral_dendrite = Cylinder(length=30*um)
+        self.ttmn_neuron_morph.axon = Cylinder(length=50*um)
+
+
+
+
+        # Sample equation for the neuron's groups, will change later
         self.eqs =  '''
             dv/dt = 1-v : 1
         '''
     
-    # Here are the neurons that will created from the number of neurons listed
-    def create_neurons(self):
-        gf_neurons = NeuronGroup(self.num_gf, self.eqs)
-        ttm_neurons = NeuronGroup(self.num_ttm, self.eqs)
-        psi_neurons = NeuronGroup(self.num_psi, self.eqs)
-        dlmn_neurons = NeuronGroup(self.num_dlmn, self.eqs)
+        # Here are the neurons that will created from the number of neurons listed
+        self.gf_neuron = SpatialNeuron(morphology=self.gf_neuron_morph)
+        self.ttm_neuron = SpatialNeuron(morphology=self.ttmn_neuron_morph)
+        self.psi_neuron = SpatialNeuron()
+        self.dlmn_neuron = SpatialNeuron()
